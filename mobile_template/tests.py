f3e79c298b4706b5fdf8c487c2e51d536a1adc49
Anton Linevich <anton@linevich.net>
2016-12-29 09:12:48 +0200

    Added tests

--- mobile_template/tests.py
+++ mobile_template/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
-# Create your tests here.
+
+class MobileViewTestCase(TestCase):
+    def test_desktop_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertIn(b'<title>Desktop template</title>', response.content)
+
+    def test_mobile_view(self):
+        response = self.client.get(reverse('home'), SERVER_NAME='m.localhost')
+        self.assertIn(b'<title>Mobile template</title>', response.content)
