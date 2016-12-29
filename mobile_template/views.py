1665983954eef85dacb2fda4dc6e8183b5461785
Anton Linevich <anton@linevich.net>
2016-12-29 08:56:07 +0200

    Implemented MobileTemplateView.

--- mobile_template/views.py
+++ mobile_template/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+import re
+from django.conf import settings
 
-# Create your views here.
+
+class MobileTemplateView(object):
+    """
+    Change template_name based on domain.
+    """
+    template_name = NotImplemented
+
+    @staticmethod
+    def is_mobile(host):
+        """
+        Check if domain is matched to regex.
+
+        :param host: hostname.
+        :type host: str
+        :rtype: bool
+        :return: true if domain is mobile.
+        """
+        if hasattr(settings, 'MOBILE_DOMAIN_REGEX'):
+            regex = re.compile(settings.MOBILE_DOMAIN_REGEX)
+        else:
+            regex = re.compile('^m.\.*.\.*.*')
+
+        if regex.match(host):
+            return True
+        else:
+            return False
+
+    def get_template_names(self):
+        """
+        Overriding default method to be able change template_name dynamically.
+
+        :rtype: list
+        :return: template names list.
+        """
+        if self.is_mobile(self.request.get_host()):
+            if hasattr(settings, 'MOBILE_TEMPLATES_PREFIX'):
+                return [settings.MOBILE_TEMPLATES_PREFIX + self.template_name, ]
+            else:
+                return ['mobile/' + self.template_name, ]
+
+        return [self.template_name]
